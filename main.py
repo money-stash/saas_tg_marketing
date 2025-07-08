d7e900e5f6f9915f0bc6a6051895a4fc0a0bc735
maksEveline <maksimeveline@gmail.com>
2025-07-08 17:44:50 +0300

    add statistic page and update navigation; enhance logs and sessions with additional details

--- main.py
+++ main.py
@@ -30,9 +30,14 @@ def open_logs():
     return render_template("logs.html")
 
 
+@app.route("/statistic")
+def open_statistic():
+    return render_template("statistic.html")
+
+
 async def main():
     await db.init_models()
-    app.run(debug=True)
+    app.run(debug=True, port=5555)
 
 
 if __name__ == "__main__":
