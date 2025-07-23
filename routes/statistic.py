d490af0c0189ee2ae456beb0ad43edf15c89ee29
maksEveline <maksimeveline@gmail.com>
2025-07-23 22:59:06 +0300

    updated styles

--- routes/statistic.py
+++ routes/statistic.py
@@ -1,8 +1,24 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
+
+from datetime import datetime
 
 statistic_bp = Blueprint("statistic", __name__)
 
 
 @statistic_bp.route("/statistic")
 async def open_statistic():
-    return render_template("statistic.html")
+    statistics = [
+        {
+            "id": 1,
+            "date": datetime.now().strftime("%Y-%m-%d"),
+            "type": "Рассылка",
+            "target_type": "Сессия",
+        },
+        {
+            "id": 2,
+            "date": datetime.now().strftime("%Y-%m-%d"),
+            "type": "Парсинг",
+            "target_type": "Задача",
+        },
+    ]
+    return render_template("statistic.html", statistics=statistics)
