384f87dbdf451710ac72d950652fb50afd4f8e08
maksEveline <maksimeveline@gmail.com>
2025-07-24 23:38:34 +0300

    mini upd

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import asyncio
 from flask import Flask
 from routes import register_blueprints
-
 from database.db import db
-
 
 app = Flask(__name__, template_folder="frontend", static_folder="frontend/static")
 app.secret_key = "super_secret_value_123"
@@ -12,7 +10,7 @@ register_blueprints(app)
 
 async def main():
     await db.init_models()
-    app.run(debug=True, port=5555)
+    app.run(host="0.0.0.0", port=5555, debug=True)
 
 
 if __name__ == "__main__":
